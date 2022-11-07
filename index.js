8a0782f64bc2cfffdf5cc256650e60a2e55c018d
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-11-07 08:42:53 -0600

    Add Socket.io to the project

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+//New imports
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
+
 app.get('/api', (req, res) => {
   res.json({
     message: 'Hello world',
