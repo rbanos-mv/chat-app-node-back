a847d716e3c809d121d975f0babae8b1e335b967
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-11-07 11:29:43 -0600

    Listen to typing events and rebroadcast them

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ socketIO.on('connection', (socket) => {
     socketIO.emit('messageResponse', data);
   });
 
+  socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
   //Listens when a new user joins the server
   socket.on('newUser', (data) => {
     //Adds the new user to the list of users
