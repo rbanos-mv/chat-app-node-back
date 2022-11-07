4b5dca546e99528b8c4ce7e90d36cb191b9fd472
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-11-07 11:28:39 -0600

    Updates the list of users and broadcast them

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const socketIO = require('socket.io')(http, {
   }
 });
 
+let users = [];
+
 socketIO.on('connection', (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
 
@@ -24,8 +26,22 @@ socketIO.on('connection', (socket) => {
     socketIO.emit('messageResponse', data);
   });
 
+  //Listens when a new user joins the server
+  socket.on('newUser', (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+  });
+
   socket.on('disconnect', () => {
     console.log('🔥: A user disconnected');
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+    socket.disconnect();
   });
 });
 
